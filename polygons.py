e67cfd48bab5b3f9706a14eec45218ef4d953367
Zavim <Zark471@gmail.com>
2021-09-17 15:29:18 -0400

    graphing zones on top of building

--- polygons.py
+++ polygons.py
@@ -47,24 +47,46 @@ class Polygons():
         return polygon_list
 
     @staticmethod
-    def parse_zones(filepath, building):
-        with open(filepath, 'r') as readFile:
-            csv_file = csv.DictReader(readFile)
-            BX3 = 49
-            BY3 = 80
-            Lb = 10
-            try:
-                for row in csv_file:
-                    formula = row['Formula']
-                    tree = ast.parse(formula)
-                    # ast solution wont work TODO: hardcode the formulas into a dict or something
-                    # max_x = building[0].exterior.coords[1][0]
-                    # max_y = building[0].exterior.coords[1][1]
-                formuler = ast.unparse(tree)
-                print(ast.literal_eval(formuler))
-            except csv.Error as e:
-                sys.exit('file {}, line {}: {}'.format(
-                    filepath, csv_file.line_num, e))
+    def calculate_zones(building):
+        BX3 = building[0].exterior.coords[1][0]
+        BY3 = building[0].exterior.coords[1][1]
+        Lb = 15
+        formulas = {'A1': {1: [BX3, BY3],
+                           2: [BX3, BY3-2*Lb],
+                           3: [BX3-2*Lb, BY3-2*Lb],
+                           4: [BX3-0.5*Lb, BY3]},
+                    'A2': {1: [BX3-0.5*Lb, BY3],
+                           2: [BX3-2*Lb, BY3-2*Lb],
+                           3: [BX3-4*Lb, BY3-2*Lb],
+                           4: [BX3-4*Lb, BY3]},
+                    'B': {1: [BX3, BY3-2*Lb],
+                          2: [BX3, BY3-4*Lb],
+                          3: [BX3-2*Lb, BY3-4*Lb],
+                          4: [BX3-2*Lb, BY3-2*Lb]},
+                    'C': {1: [BX3-4*Lb, BY3],
+                          2: [BX3-4*Lb, BY3-2*Lb],
+                          3: [BX3-2*Lb, BY3-2*Lb],
+                          4: [BX3-2*Lb, BY3-4*Lb],
+                          5: [BX3, BY3-4*Lb],
+                          6: [BX3, BY3-6*Lb],
+                          7: [BX3-4*Lb, BY3-6*Lb],
+                          8: [BX3-4*Lb, BY3-4*Lb],
+                          9: [BX3-6*Lb, BY3-4*Lb],
+                          10: [BX3-6*Lb, BY3]}}
+        zones = []
+        for values in formulas.values():
+            zones.append(values)
+        zone_polygon = []
+        iter_row = []
+        for zone in zones:
+            values = zone.values()
+            for i in iter(values):
+                iter_row.append(i)
+            zone_polygon.append(iter_row)
+            # ax.add_artist(PolygonPatch(zone_polygon, alpha=.5))
+            iter_row = []
+        zones = Polygons.build_polygons(zone_polygon)
+        return zones
 
     @staticmethod
     def graph_polygons(building=None, zones=None, panels=None, show=True):
@@ -79,15 +101,28 @@ class Polygons():
         max_y = building[0].exterior.coords[1][1]
         ax.set_xlim(0, max_x)
         ax.set_ylim(0, max_y)
-        # col = _plot_polygon_collection(ax, p.geometry)
-        # col.set_array(np.array([0, 1, 0]))
-        # triangle = polygon_list[0]
-        # square = polygon_list[1]
-        # print(square)
-        for polygon in building:
-            ax.add_artist(PolygonPatch(polygon, alpha=.25))
-        for polygon in zones:
-            ax.add_artist(PolygonPatch(polygon, alpha=.5))
+
+        if building:
+            for polygon in building:
+                ax.add_artist(PolygonPatch(polygon, alpha=.25))
+        if zones:
+            for polygon in zones:
+                ax.add_artist(PolygonPatch(polygon, alpha=.5))
+            # zone_polygon = []
+            # iter_row = []
+            # for zone in zones:
+            #     # zone_coordinates.append(zone.values())
+            #     # print(list(iter(zone)))
+            #     values = zone.values()
+            #     for i in iter(values):
+            #         iter_row.append(i)
+            #     zone_polygon.append(iter_row)
+            #     # ax.add_artist(PolygonPatch(zone_polygon, alpha=.5))
+            #     iter_row = []
+            #     # zone_polygon = []
+            # print(zone_polygon)
+            # print(list(iter(zone_coordinates)))
+            # ax.add_artist(PolygonPatch(polygon, alpha=.5))
         # for polygon in panels:
         # panel coordinates are calculated from the center of the panel,
         # bottom left of the roof
@@ -111,11 +146,9 @@ def main():
     building_coordinates = Polygons.parse_csv(building_filepath)
     # zones_coordinates = Polygons.parse_csv(zones_filepath)
     building = Polygons.build_polygons(building_coordinates)
-    # zones = Polygons.build_polygons(zones_coordinates)
-    # graph = Polygons.graph_polygons(building, zones)
+    zones = Polygons.calculate_zones(building)
+    graph = Polygons.graph_polygons(building, zones)
     # intersection = Polygons.calculate_intersection(series)
-
-    formulas = Polygons.parse_zones(zone_formulas_filepath, building)
     # print(formulas)
 
 
