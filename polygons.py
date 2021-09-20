9718b2ba1fc4a61e5f51bd36138c308ec9b2e0a7
Zavim <Zark471@gmail.com>
2021-09-20 17:25:12 -0400

    intersection basics

--- polygons.py
+++ polygons.py
@@ -11,7 +11,9 @@ from descartes import PolygonPatch
 
 zones_filepath = 'csv/zones.csv'
 zone_formulas_filepath = 'csv/zoneFormulas.csv'
-building_filepath = 'csv/building.csv'
+building_filepath = 'csv/bigBuilding.csv'
+array_filepath = 'csv/array.csv'
+# building_filepath = 'csv/building.csv'
 # coordinates_filepath = 'csv/coordinates.csv'
 # coordinates_filepath = 'csv/intersection.csv'
 
@@ -48,48 +50,127 @@ class Polygons():
 
     @staticmethod
     def calculate_zones(building):
+        BX1 = building[0].exterior.coords[3][0]
+        BY1 = building[0].exterior.coords[3][1]
+        BX2 = building[0].exterior.coords[0][0]
+        BY2 = building[0].exterior.coords[0][1]
         BX3 = building[0].exterior.coords[1][0]
         BY3 = building[0].exterior.coords[1][1]
+        BX4 = building[0].exterior.coords[2][0]
+        BY4 = building[0].exterior.coords[2][1]
         Lb = 15
-        formulas = {'A1': {1: [BX3, BY3],
-                           2: [BX3, BY3-2*Lb],
+        formulas = {'3A1': {1: [BX3, BY3],
+                            2: [BX3, BY3-2*Lb],
+                            3: [BX3-2*Lb, BY3-2*Lb],
+                            4: [BX3-0.5*Lb, BY3]},
+                    '3A2': {1: [BX3-0.5*Lb, BY3],
+                            2: [BX3-2*Lb, BY3-2*Lb],
+                            3: [BX3-4*Lb, BY3-2*Lb],
+                            4: [BX3-4*Lb, BY3]},
+                    '3B': {1: [BX3, BY3-2*Lb],
+                           2: [BX3, BY3-4*Lb],
+                           3: [BX3-2*Lb, BY3-4*Lb],
+                           4: [BX3-2*Lb, BY3-2*Lb]},
+                    '3C': {1: [BX3-4*Lb, BY3],
+                           2: [BX3-4*Lb, BY3-2*Lb],
                            3: [BX3-2*Lb, BY3-2*Lb],
-                           4: [BX3-0.5*Lb, BY3]},
-                    'A2': {1: [BX3-0.5*Lb, BY3],
-                           2: [BX3-2*Lb, BY3-2*Lb],
-                           3: [BX3-4*Lb, BY3-2*Lb],
-                           4: [BX3-4*Lb, BY3]},
-                    'B': {1: [BX3, BY3-2*Lb],
-                          2: [BX3, BY3-4*Lb],
-                          3: [BX3-2*Lb, BY3-4*Lb],
-                          4: [BX3-2*Lb, BY3-2*Lb]},
-                    'C': {1: [BX3-4*Lb, BY3],
-                          2: [BX3-4*Lb, BY3-2*Lb],
-                          3: [BX3-2*Lb, BY3-2*Lb],
-                          4: [BX3-2*Lb, BY3-4*Lb],
-                          5: [BX3, BY3-4*Lb],
-                          6: [BX3, BY3-6*Lb],
-                          7: [BX3-4*Lb, BY3-6*Lb],
-                          8: [BX3-4*Lb, BY3-4*Lb],
-                          9: [BX3-6*Lb, BY3-4*Lb],
-                          10: [BX3-6*Lb, BY3]}}
+                           4: [BX3-2*Lb, BY3-4*Lb],
+                           5: [BX3, BY3-4*Lb],
+                           6: [BX3, BY3-6*Lb],
+                           7: [BX3-4*Lb, BY3-6*Lb],
+                           8: [BX3-4*Lb, BY3-4*Lb],
+                           9: [BX3-6*Lb, BY3-4*Lb],
+                           10: [BX3-6*Lb, BY3]},
+                    '4E': {1: [BX4-5*Lb, BY4+2*Lb],
+                           2: [BX4-5*Lb, BY4],
+                           3: [BX1, BY1],
+                           4: [BX1, BY1+3*Lb],
+                           5: [BX4-7*Lb, BY4+3*Lb],
+                           6: [BX4-7*Lb, BY4+2*Lb]},
+                    '4F1': {1: [BX4, BY4],
+                            2: [BX4-2*Lb, BY4],
+                            3: [BX4-2*Lb, BY4+Lb],
+                            4: [BX4, BY4+0.5*Lb]},
+                    '4F2': {1: [BX4-2*Lb, BY4+2*Lb],
+                            2: [BX4-2*Lb, BY4],
+                            3: [BX4-5*Lb, BY4],
+                            4: [BX4-5*Lb, BY4+2*Lb]},
+                    '4G1': {1: [BX4, BY4+3*Lb],
+                            2: [BX4, BY4+0.5*Lb],
+                            3: [BX4-2*Lb, BY4+Lb],
+                            4: [BX4-2*Lb, BY4+3*Lb]},
+                    '4G2': {1: [BX4, BY4+6*Lb],
+                            2: [BX4, BY4+3*Lb],
+                            3: [BX4-2*Lb, BY4+3*Lb],
+                            4: [BX4-2*Lb, BY4+2*Lb],
+                            5: [BX4-7*Lb, BY4+2*Lb],
+                            6: [BX4-7*Lb, BY4+4*Lb],
+                            7: [BX4-2*Lb, BY4+4*Lb],
+                            8: [BX4-2*Lb, BY4+6*Lb]},
+                    '2A1': {1: [BX2, BY2],
+                            2: [BX2+0.5*Lb, BY2],
+                            3: [BX2+2*Lb, BY2-2*Lb],
+                            4: [BX2, BY2-2*Lb]},
+                    '2A2': {1: [BX2+0.5*Lb, BY2],
+                            2: [BX2+4*Lb, BY2],
+                            3: [BX2+4*Lb, BY2-2*Lb],
+                            4: [BX2+2*Lb, BY2-2*Lb]},
+                    '2B': {1: [BX2, BY2-2*Lb],
+                           2: [BX2+2*Lb, BY2-2*Lb],
+                           3: [BX2+2*Lb, BY2-4*Lb],
+                           4: [BX2, BY2-4*Lb]},
+                    '2C': {1: [BX2+4*Lb, BY2],
+                           2: [BX2+6*Lb, BY2],
+                           3: [BX2+6*Lb, BY2-4*Lb],
+                           4: [BX2+4*Lb, BY2-4*Lb],
+                           5: [BX2+4*Lb, BY2-6*Lb],
+                           6: [BX2, BY2-6*Lb],
+                           7: [BX2, BY2-4*Lb],
+                           8: [BX2+2*Lb, BY2-4*Lb],
+                           9: [BX2+2*Lb, BY2-2*Lb],
+                           10: [BX2+4*Lb, BY2-2*Lb]},
+                    '1E': {1: [BX1+5*Lb, BY1],
+                           2: [BX1+5*Lb, BY1+2*Lb],
+                           3: [BX1+7*Lb, BY1+2*Lb],
+                           4: [BX1+7*Lb, BY1+3*Lb],
+                           5: [BX4, BY4+3*Lb],
+                           6: [BX4, BY4]},
+                    '1F1': {1: [BX1, BY1],
+                            2: [BX1, BY1+0.5*Lb],
+                            3: [BX1+2*Lb, BY1+1*Lb],
+                            4: [BX1+2*Lb, BY1]},
+                    '1F2': {1: [BX1+2*Lb, BY1],
+                            2: [BX1+2*Lb, BY1+2*Lb],
+                            3: [BX1+5*Lb, BY1+2*Lb],
+                            4: [BX1+5*Lb, BY1]},
+                    '1G1': {1: [BX1, BY1+0.5*Lb],
+                            2: [BX1, BY1+3*Lb],
+                            3: [BX1+2*Lb, BY1+3*Lb],
+                            4: [BX1+2*Lb, BY1+1*Lb]},
+                    '1G2': {1: [BX1, BY1+3*Lb],
+                            2: [BX1, BY1+6*Lb],
+                            3: [BX1+2*Lb, BY1+6*Lb],
+                            4: [BX1+2*Lb, BY1+4*Lb],
+                            5: [BX1+7*Lb, BY1+4*Lb],
+                            6: [BX1+7*Lb, BY1+2*Lb],
+                            7: [BX1+2*Lb, BY1+2*Lb],
+                            8: [BX1+2*Lb, BY1+3*Lb]}}
         zones = []
         for values in formulas.values():
             zones.append(values)
-        zone_polygon = []
+        zone_polygons = []
         iter_row = []
         for zone in zones:
             values = zone.values()
             for i in iter(values):
                 iter_row.append(i)
-            zone_polygon.append(iter_row)
-            # ax.add_artist(PolygonPatch(zone_polygon, alpha=.5))
+            zone_polygons.append(iter_row)
             iter_row = []
-        zones = Polygons.build_polygons(zone_polygon)
+        zones = Polygons.build_polygons(zone_polygons)
         return zones
 
     @staticmethod
-    def graph_polygons(building=None, zones=None, panels=None, show=True):
+    def graph_polygons(building=None, zones=None, array=None, show=True):
         # p = gpd.GeoSeries(polygon_list)
         # fig, ax = plt.subplots(subplot_kw=dict(aspect='equal'))
         # col = _plot_polygon_collection(ax, p.geometry)
@@ -108,47 +189,47 @@ class Polygons():
         if zones:
             for polygon in zones:
                 ax.add_artist(PolygonPatch(polygon, alpha=.5))
-            # zone_polygon = []
-            # iter_row = []
-            # for zone in zones:
-            #     # zone_coordinates.append(zone.values())
-            #     # print(list(iter(zone)))
-            #     values = zone.values()
-            #     for i in iter(values):
-            #         iter_row.append(i)
-            #     zone_polygon.append(iter_row)
-            #     # ax.add_artist(PolygonPatch(zone_polygon, alpha=.5))
-            #     iter_row = []
-            #     # zone_polygon = []
-            # print(zone_polygon)
-            # print(list(iter(zone_coordinates)))
-            # ax.add_artist(PolygonPatch(polygon, alpha=.5))
-        # for polygon in panels:
-        # panel coordinates are calculated from the center of the panel,
-        # bottom left of the roof
-        #     ax.add_artist(PolygonPatch(polygon, alpha=.5))
+        if array:
+            for polygon in array:
+                ax.add_artist(PolygonPatch(polygon, alpha=.75))
         if show:
             plt.show()
 
     @staticmethod
-    def calculate_intersection(series):
-        for i in range(len(series)):
-            for j in range(1, len(series)):
-                if not j == i:
-                    intersects = (series[i].intersects(series[j]))
-                    if intersects:
-                        intersection = (series[i].intersection(series[j]))
-                        if intersection.area > 0.0:
-                            print(intersection.area)
+    def calculate_intersection(array, zones):
+        for panel in iter(array):
+            for zone in iter(zones):
+                intersects = panel.intersects(zone)
+                if intersects:
+                    intersection = (panel.intersection(zone))
+                    if intersection.area > 0.0:
+                        print(intersection.area, zone)
+        # for i in range(len(series)):
+        #     for j in range(1, len(series)):
+        #         if not j == i:
+        #             intersects = (series[i].intersects(series[j]))
+        #             if intersects:
+        #                 intersection = (series[i].intersection(series[j]))
+        #                 if intersection.area > 0.0:
+        #                     print(intersection.area)
+        # intersects = (*array.intersects(*zones))
+        # if intersects:
+        #     intersection = (array.intersection(zones))
+        #     if intersection.area > 0.0:
+        #         print(intersection.area)
+        # print(*array)
+        # print(*zones)
 
 
 def main():
     building_coordinates = Polygons.parse_csv(building_filepath)
-    # zones_coordinates = Polygons.parse_csv(zones_filepath)
+    array_coordinates = Polygons.parse_csv(array_filepath)
     building = Polygons.build_polygons(building_coordinates)
+    array = Polygons.build_polygons(array_coordinates)
     zones = Polygons.calculate_zones(building)
-    graph = Polygons.graph_polygons(building, zones)
-    # intersection = Polygons.calculate_intersection(series)
+    # print(zones)
+    graph = Polygons.graph_polygons(building, zones, array, show=True)
+    intersection = Polygons.calculate_intersection(array, zones)
     # print(formulas)
 
 
